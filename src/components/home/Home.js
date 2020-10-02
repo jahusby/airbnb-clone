b9eacbce299acef377fbc01c58a8c0908e665dae
jahusby <32870250+jahusby@users.noreply.github.com>
2020-10-02 08:34:28 +0200

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -10,28 +10,41 @@ function Home() {
 
       <div className="home__section">
         <Card
-          src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidthier%2Ffiles%2F2018%2F07%2FSquirtle_Squad.jpg"
-          title="squirtle"
-          description="squirtle squad fam"
-          price="50 dolla/night"
+          src="https://cf.bstatic.com/images/hotel/max1024x768/232/232147488.jpg"
+          title="Unique stays"
+          description="Spaces that are more than just a place to sleep"
         />
         <Card
-          src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidthier%2Ffiles%2F2018%2F07%2FSquirtle_Squad.jpg"
-          title="squirtle"
-          description="squirtle squad fam"
-          price="50 dolla/night"
+          src="https://skift.com/wp-content/uploads/2019/12/airbnb-plus-room-cape-town-1024x684.jpg"
+          title="Online Experiences"
+          description="A new way to travel from home"
         />
         <Card
-          src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidthier%2Ffiles%2F2018%2F07%2FSquirtle_Squad.jpg"
-          title="squirtle"
-          description="squirtle squad fam"
-          price="50 dolla/night"
+          src="https://beyondphilosophy.com/wp-content/uploads/2019/10/0.jpg"
+          title="Entire homes"
+          description="Comfortable private places, with room for friends or family"
         />
       </div>
+
       <div className="home__section">
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          src="https://www.itromso.no/nyhet/article20066947.ece/u18qna/ALTERNATES/w980-default/31NYHleilighetillustrasjon.jpg"
+          title="3 bedroom flat in"
+          description="Spaces that are more than just a place to sleep"
+          price="$100/night"
+        />
+        <Card
+          src="https://www.itromso.no/nyhet/article20066947.ece/u18qna/ALTERNATES/w980-default/31NYHleilighetillustrasjon.jpg"
+          title="Penthouse in Oslo"
+          description="Enjoy the amazing sights of Oslo with this stunning penthouse"
+          price="$300/night"
+        />
+        <Card
+          src="https://www.itromso.no/nyhet/article20066947.ece/u18qna/ALTERNATES/w980-default/31NYHleilighetillustrasjon.jpg"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="$70/night"
+        />
       </div>
     </div>
   );
